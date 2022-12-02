35bade4c57f3e1ad08d5588d4f4cb517ac463fdc
adyanshkakran <adyanshkakran@gmail.com>
2022-12-02 13:02:41 +0530

    Added instructions bar

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -1,1 +1,10 @@
-//Your JavaScript goes in here
+function showInstructions() {
+    let ele = document.getElementsByClassName("instruction-button")[0]
+    ele.classList.toggle("active");
+    let content = ele.nextElementSibling;
+    if (content.style.maxHeight){
+        content.style.maxHeight = null;
+    } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+    }
+}
